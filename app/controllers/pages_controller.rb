5fd1be56020b61522c7ceed8c1fa6b5f2b7f4bc0
Tonatiuh Nunez <tonatiuhn@hotmail.es>
2013-12-27 17:00:41 -0600

    add styled header layout (for logged in users)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,4 @@
 class PagesController < ApplicationController
-
   def index
     unless user_signed_in?
       render template: 'pages/landing_page', layout: 'welcome'
@@ -7,8 +6,8 @@ class PagesController < ApplicationController
   end
 
   private
-    def user_signed_in?
-      false
-    end
 
+  def user_signed_in?
+    false
+  end
 end
