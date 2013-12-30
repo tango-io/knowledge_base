ec9d42810ab236951bdd8fbcfc8481b49cebad45
Cesar Gomez <cesargomez89@gmail.com>
2013-12-30 14:11:19 -0600

    added backend and routes for document

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,50 +1,20 @@
 class DocumentsController < ApplicationController
-  before_filter :find_document, only: [:edit, :update, :show, :destroy]
-
-  def new
-    @document = Document.new()
-  end
-
-  def show
-  end
+  expose(:document)
 
   def create
-    @document = Document.new(doc_params)
-    if @document.save
-      redirect_to @document
+    if document.save
+      redirect_to document
     else
       render :new
     end
   end
 
-  def edit
-  end
-
   def update
-    if @document.update(doc_params)
-      redirect_to(@document)
+    if document.save
+      redirect_to document
     else
       render :edit
     end
-  end
-
-  def destroy
-    if @document.destroy
-      redirect_to root_path
-    else
-      redirect_to root_path
-      flash.now[:alert] = 'Error while deleting document!'
-    end
-  end
-
-  private
-
-  def find_document
-    @document = Document.find(params[:id])
-  end
-
-  def doc_params
-    params.require(:document).permit(:title, :body)
   end
 
 end
