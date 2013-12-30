406ea20e9d26c3bf72918920df4ce52c10d4bf78
Cesar Gomez <cesargomez89@gmail.com>
2013-12-30 12:47:40 -0600

    added front tests documents

---
+++ spec/features/documents/create_document_spec.rb
@@ -0,0 +1,19 @@
+include 'spec_helper'
+
+feature 'As a user I can create a document' do
+  before do
+    @title = Faker::Lorem.sentence
+    @body  = Faker::Lorem.paragraphs
+
+    visit "/"
+  end
+  scenario 'create document' do
+    click_button('Create document')
+    fill_in('title', with: @title)
+    fill_in('body', with: @body)
+    click_button('Save')
+
+    expect(Document.count).to be_eql(1)
+    expect(current_path).to be_eql(document_path(Document.first))
+  end
+end
