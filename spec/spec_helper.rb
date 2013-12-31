e147470eb193ec382a969f505214f35c8599c39c
Tonatiuh Nunez <tonatiuhn@hotmail.es>
2013-12-31 01:28:44 -0600

    missing file from improving some spec files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,13 +4,26 @@ require 'rspec/rails'
 require 'rspec/autorun'
 require 'capybara/rails'
 
-Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
-
+Dir["#{::Rails.root}/spec/support/factories/**/*.rb"].each{ |file| load "#{file}" }
 ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
 Capybara.javascript_driver = :webkit
 
 RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+
   config.infer_base_class_for_anonymous_controllers = false
   config.order = "random"
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
