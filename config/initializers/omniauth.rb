6e53c46a35d433b33ec814557cb9e488bec89845
Tonatiuh Nunez <tonatiuhn@hotmail.es>
2013-12-31 09:45:13 -0600

    add google_credentials.yml

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,10 @@
-GOOGLE_CREDENTIALS = YAML.load_file(Rails.root.join('config/google_credentials.yml'))[Rails.env]
+yaml_file_route = if File.exists?('config/google_credentials.local.yml')
+                    'config/google_credentials.local.yml'
+                  else
+                    'config/google_credentials.yml'
+                  end
+
+GOOGLE_CREDENTIALS = YAML.load_file(Rails.root.join(yaml_file_route))[Rails.env]
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider(
