98f9889d57ba4975ce5619bdfad5b41bf826fc93
Cesar Gomez <cesargomez89@gmail.com>
2014-01-02 09:24:02 -0600

    added delete action with tests

--- spec/controllers/documents_controller_spec.rb
+++ spec/controllers/documents_controller_spec.rb
@@ -28,5 +28,18 @@ describe DocumentsController do
       expect(response.request.fullpath).to eq(document_path(document))
     end
 
+    it 'delete a document' do
+      delete :destroy, { id: document.id }
+      expect(Document.count).to eq(0)
+    end
+
+    it 'show message if cannot delete the document' do
+      allow_any_instance_of(Document).to receive(:destroy).and_return(false)
+      delete :destroy, { id: document.id }
+
+      expect(Document.count).to eq(1)
+      expect(flash[:alert]).to eq('Error while deleting document!')
+    end
+
   end
 end
