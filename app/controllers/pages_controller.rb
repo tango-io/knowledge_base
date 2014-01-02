4e002e7483b4f75bb19dc0a2040d556c1e0651a6
Cesar Gomez <cesargomez89@gmail.com>
2014-01-02 07:57:21 -0600

    fixed tests and css

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,13 @@
 class PagesController < ApplicationController
   def index
-    @documents = Document.all
-    unless user_signed_in?
-      return render template: 'pages/landing_page', layout: 'welcome'
+    if user_signed_in?
+      @documents = Document.all
+    else
+      render template: 'pages/landing_page', layout: 'welcome'
     end
 
     @user = User.find(session[:user_id])
   end
-
-  private
 
   def user_signed_in?
     session[:user_id]
--- spec/features/documents/show_document_spec.rb
+++ spec/features/documents/show_document_spec.rb
@@ -4,12 +4,15 @@ feature 'As a user I can see a document' do
   let!(:document) { create :document }
 
   before do
-    allow(PagesController).to receive(:user_signed_in?).and_return(true)
+    @documents = Document.all
+    allow_any_instance_of(PagesController).to receive(:user_signed_in?).and_return(true)
     visit root_path
   end
 
   scenario 'show document' do
-    click_link(document.title)
+    within('.documents') do
+      click_link(document.title)
+    end
 
     expect(page.body).to have_content(document.body)
   end
