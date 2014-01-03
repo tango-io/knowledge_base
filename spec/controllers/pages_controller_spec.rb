f5e3bc8412e932427bbd499f30023313832565c7
Cesar Gomez <cesargomez89@gmail.com>
2014-01-03 05:00:05 -0600

    added tests

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -19,15 +19,21 @@ describe PagesController, 'index template' do
   context 'if there is a user session active' do
     before do
       allow(controller).to receive(:user_signed_in?).and_return(true)
-      get :index
     end
 
     it 'renders the index view' do
+      get :index
       expect(response).to render_template('index')
     end
 
     it 'renders the view with application layout' do
+      get :index
       expect(response).to render_template(layout: 'application')
+    end
+
+    it 'if there is a search param invoke search_by_title method of Document' do
+      get :index, { search: "something"}
+      expect(Document).to receive(:search_by_title).once
     end
   end
 
