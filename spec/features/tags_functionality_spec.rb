8af9a21fc7069b3afd8fefc7f79cc514ddfa9fb9
Cesar Gomez <cesargomez89@gmail.com>
2014-01-03 20:54:45 -0600

    added tests for popular tags

--- spec/features/tags_functionality_spec.rb
+++ spec/features/tags_functionality_spec.rb
@@ -5,22 +5,23 @@ def login_to_google(mock_options)
   visit '/auth/google_oauth2'
 end
 
-def create_popular_tags(tags)
-  (1..10) do |i|
-    Document.create()
+def create_popular_tags
+  (1..10).map do |i|
+    Document.create( title: Faker::Lorem.sentence, tag_list: i.to_s )
+    Document.create( title: Faker::Lorem.sentence, tag_list: i.to_s )
   end
 end
 
-def create_not_popular_tags(tags)
-  (11..20) do |i|
-
+def create_not_popular_tags
+  (11..20).map do |i|
+    Document.create( title: Faker::Lorem.sentence, tag_list: i.to_s )
   end
 end
 
 feature 'Tags functionality' do
   context 'at the home page' do
-    let!(:popular_tags){ Faker::Lorem.words(10) }
-    let!(:not_popular_tags){ Faker::Lorem.words(10) }
+    let!(:popular_tags) { Document.popular_tags }
+
     let!(:user) do
       create(
         :tango_user,
@@ -30,11 +31,16 @@ feature 'Tags functionality' do
     end
 
     before do
+      create_popular_tags
+      create_not_popular_tags
       login_to_google(uid: user.uid, info: { email: user.email }, credentials: {})
     end
 
     scenario 'user sees the list of the 10 most popular tags' do
       visit root_path
+      popular_tags.each do |tag|
+        expect(page.body).to include(tag.name)
+      end
     end
   end
 end
--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -7,11 +7,31 @@ describe Document do
 
   context 'Methods' do
     let!(:document) { create :document }
+
     it 'search' do
       title = Faker::Lorem.sentence
       document2 = create(:document, title: title)
       expect(Document.search(title)).to include(document2)
     end
+
+    it 'popular_tags' do
+      (1..10).map do |i|
+        Document.create( title: Faker::Lorem.sentence, tag_list: i.to_s )
+        Document.create( title: Faker::Lorem.sentence, tag_list: i.to_s )
+      end
+
+      (11..20).map do |i|
+        Document.create( title: Faker::Lorem.sentence, tag_list: i.to_s )
+      end
+
+      expect(Document.popular_tags).to eq(
+      Document.tag_counts_on(
+        :tags,
+        :limit => 10,
+        :order => "count desc")
+      )
+    end
+
   end
 
 end
