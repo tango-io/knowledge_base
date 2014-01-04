530fc2b822bf070b0eb7e403267c9cbc9b24c3f6
Tonatiuh Nunez <tonatiuhn@hotmail.es>
2014-01-04 01:48:17 -0600

    add integration specs corresponding to the document tag search by title and tag

--- spec/features/pages/search_document_spec.rb
+++ spec/features/pages/search_document_spec.rb
@@ -2,32 +2,34 @@ require 'spec_helper'
 
 def login_to_google(mock_options)
   OmniAuth.config.add_mock :google_oauth2, mock_options
-
   visit '/auth/google_oauth2'
 end
 
-feature 'Search documents' do
-  let!(:title) { Faker::Lorem.sentence }
-  let!(:document) { create :document, title: title }
-  let!(:document2) { create :document }
-  let!(:user) do
-    create(
-      :tango_user,
-      image: Faker::Internet.url,
-      name: Faker::Name.name
-    )
-  end
+feature 'Document search' do
+  let!(:title){ Faker::Lorem.sentence }
+  let!(:document){ create :document, title: title }
+  let!(:document2){ create :document }
+  let!(:user){ create :tango_user }
 
   before do
     login_to_google(uid: user.uid, info: { email: user.email }, credentials: {})
     visit root_path
   end
 
-  scenario 'As a user I can search a document' do
-    fill_in('search', with: title)
-    click_button('Search')
+  context 'when the user is searching for a document by title' do
+    scenario 'user finds the document if it exists' do
+      fill_in('search', with: title)
+      click_button('Search')
 
-    expect(page.body).to include(document.title)
-    expect(page.body).to_not include(document2.title)
+      expect(page).to have_content(document.title)
+      expect(page).to_not have_content(document2.title)
+    end
+
+    scenario 'user sees the looked title displayed' do
+      fill_in('search', with: title)
+      click_button('Search')
+
+      find('h5', text: title)
+    end
   end
 end
